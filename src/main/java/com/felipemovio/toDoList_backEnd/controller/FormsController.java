1289396cdd0903a3de1240bdfe157ca568d6e6d2
FelipeMovio <felipelsmovio@gmail.com>
2025-10-15 15:47:03 -0300

    começando o controller

--- src/main/java/com/felipemovio/toDoList_backEnd/controller/FormsController.java
+++ src/main/java/com/felipemovio/toDoList_backEnd/controller/FormsController.java
@@ -1,10 +1,16 @@
 package com.felipemovio.toDoList_backEnd.controller;
 
 
+import com.felipemovio.toDoList_backEnd.service.FormsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/forms")
 public class FormsController {
+
+    @Autowired
+    private FormsService formsService;
+
 }
