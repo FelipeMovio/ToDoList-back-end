8177b0d9c5edec57b228a079462ce7cec601f93e
FelipeMovio <felipelsmovio@gmail.com>
2025-10-15 16:26:10 -0300

    Criando Controller e add dependencia de documentação swagger

--- src/main/java/com/felipemovio/toDoList_backEnd/controller/FormsController.java
+++ src/main/java/com/felipemovio/toDoList_backEnd/controller/FormsController.java
@@ -1,10 +1,14 @@
 package com.felipemovio.toDoList_backEnd.controller;
 
 
+import com.felipemovio.toDoList_backEnd.dto.FormsRequestDTO;
+import com.felipemovio.toDoList_backEnd.dto.FormsResponseDTO;
 import com.felipemovio.toDoList_backEnd.service.FormsService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/forms")
@@ -13,4 +17,20 @@ public class FormsController {
     @Autowired
     private FormsService formsService;
 
+    @GetMapping
+    public List<FormsResponseDTO> listarTodos() {
+        return formsService.listarTodos();
+    }
+
+    @PostMapping
+    public ResponseEntity<FormsResponseDTO> criar(@RequestBody FormsRequestDTO dto) {
+        return ResponseEntity.ok(formsService.criar(dto));
+    }
+
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletar(@PathVariable Long id) {
+        formsService.deletar(id);
+        return ResponseEntity.noContent().build();
+    }
 }
