7de7ee98f0f88d696eefaf4e467c2977b769e22b
FelipeMovio <felipelsmovio@gmail.com>
2025-10-15 15:45:59 -0300

    ja colocando uma classe de config do cors

---
+++ src/main/java/com/felipemovio/toDoList_backEnd/controller/FormsController.java
@@ -0,0 +1,10 @@
+package com.felipemovio.toDoList_backEnd.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/forms")
+public class FormsController {
+}
