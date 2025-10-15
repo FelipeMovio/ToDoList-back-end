4982a0125becfb3aaff0f403d5fd88e022e96923
FelipeMovio <felipelsmovio@gmail.com>
2025-10-15 15:30:24 -0300

    criando repository

---
+++ src/main/java/com/felipemovio/toDoList_backEnd/repository/FormsRepository.java
@@ -0,0 +1,7 @@
+package com.felipemovio.toDoList_backEnd.repository;
+
+import com.felipemovio.toDoList_backEnd.model.Forms;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FormsRepository extends JpaRepository<Forms, Long> {
+}
