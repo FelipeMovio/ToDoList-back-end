6933c757b01c2418e36a17a8f15b3c88b19ac5ad
FelipeMovio <felipelsmovio@gmail.com>
2025-10-16 16:43:57 -0300

    colocando a explicação de alogumas anotações

--- src/main/java/com/felipemovio/toDoList_backEnd/dto/FormsRequestDTO.java
+++ src/main/java/com/felipemovio/toDoList_backEnd/dto/FormsRequestDTO.java
@@ -6,11 +6,14 @@ import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
+// essa anotação gera getters setters toString equals e hashcode automaticamente
 public class FormsRequestDTO {
     @NotBlank
+    //Essa anotação indica que a propriedade (campo) não pode ser nula, vazia, nem conter apenas espaços em branco.
     private String titulo;
 
     @NotNull
+    //Indica que o campo não pode ser null, ou seja, é obrigatório enviar algum valor.
     private Categoria categoria;
 
     @NotNull
